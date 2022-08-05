787cfb4b9d20d1c93e02e6222cd213496a1b2822
Dillon Dolejsi <dillon41393@sbcglobal.net>
2022-08-05 14:11:35 -0500

    changes 8/5

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,38 +1,69 @@
-from itertools import count
+# Import Dependencies
 import os
 import csv
 
+# Create the path for the CSV file
 budget_csv = os.path.join('Resources/budget_data.csv')
+
+# Create the list to store your data
 month_of_change = []
 net_change_list= []
-total_net = 0
-total_months =0
 
+# Create your variables and giving them a starting point
+total_net = 0
+total_months = 0
+
+# Open your CSV file using the path you created
 with open(budget_csv) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
+    
+# Identify the header and the following row after it
     header = next(csvreader)
     first_row = next(csvreader)
+
+# Create additional variable needed prior to the loop
     total_net += int(first_row[1])
     prev_net = int(first_row[1])
     total_months +=1
-    
+
+# Start your for loop to look through the data   
     for row in csvreader:
+
+# Looking through the total months data and saving the data to the month_of_change list
         total_months +=1
-        total_net += int(row[1]) 
         month_of_change += [row[0]]
+        
+# Calculate the net change and then saving it to our list
+        total_net += int(row[1]) 
         net_change = int(row[1])- prev_net
         prev_net = int(row[1])
         net_change_list += [net_change]
 
+# Calculate the average of the changes in Profits/Losses over the entire period and round that average 2 places
     net_monthly_change = sum(net_change_list) / len(net_change_list)
+    rounded_monthly_change = round(net_monthly_change,2)
 
+# Calculate the max and min value of the net change and the corresponding date it occured
     greatest_increase = max(net_change_list)
     greatest_decrease = min(net_change_list)
+    greatest_increase_month = month_of_change[net_change_list.index(greatest_increase)]
+    greatest_decrease_month = month_of_change[net_change_list.index(greatest_decrease)]
 
+# Print my results
+    print(f"Financial Analysis")
+    print(f"----------------------------")
     print(f"Total Months: {total_months}")
     print(f"Total: {total_net}")
-    print(f"Average Change: {net_monthly_change}")
-    print(f"Greatest Increase in Profits:  {(greatest_increase)}")
-    print(f"Greatest Decrease in Profits:  {(greatest_decrease)}")
+    print(f"Average Change: {rounded_monthly_change}")
+    print(f"Greatest Increase in Profits: {greatest_increase_month} (${greatest_increase})")
+    print(f"Greatest Decrease in Profits: {greatest_decrease_month} (${greatest_decrease})")
 
-    
+# Create a txt file with my results and export it
+with open('analysis.txt', 'w' ) as datafile:
+    datafile.write(f"Financial Analysis \n")
+    datafile.write(f"----------------------------\n")
+    datafile.write(f"Total Months: {total_months} \n")
+    datafile.write(f"Total: {total_net} \n")
+    datafile.write(f"Average Change: {rounded_monthly_change} \n")
+    datafile.write(f"Greatest Increase in Profits: {greatest_increase_month} (${greatest_increase}) \n")
+    datafile.write(f"Greatest Decrease in Profits: {greatest_decrease_month} (${greatest_decrease}) \n")
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -3,28 +3,63 @@ import csv
 
 csvpath = os.path.join('Resources/election_data.csv')
 
-def voting_results(voting_data):
-    voting_id = str(voting_data[0])
-    county = str(voting_data[1])
-    candidate = str(voting_data[2])
+voting_id = []
+county = []
+candidates_all = []
+candidate_condensed = []
+votes_counter = []
+voting_percent = []
 
-    total_votes = len(voting_id)
-    candidate_name = len(candidate)
-    candidate_votes = int(candidate.count(candidate))
-    vote_percentage = (candidate_votes / total_votes) * 100
-    winner = max(candidate_votes)
-
-    print(f"Election Results")
-    print(f"------------------------------")
-    print(f"Total Votes: {total_votes}")
-    print(f"------------------------------")
-    print(f"{candidate_name}: {vote_percentage} ({candidate_votes})")
-    print(f"------------------------------")
-    print(f"Winner: {winner}")
 
 
 with open(csvpath, encoding="utf-8") as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
 
+    header = next(csvreader)
+# Adding all of the data in the voting_is, county and candidates_all lists
     for row in csvreader:
-        voting_results(row)
+        voting_id.append(row[0])
+        county.append(row[1])
+        candidates_all.append(row[2])
+
+    total_votes = len(voting_id)
+
+# Removed duplicates from the candidiate all list to get a list of all the unique candidates   
+    for x in range(total_votes-1):
+        if candidates_all[x+1] != candidates_all[x] and candidates_all[x+1] not in candidate_condensed:
+            candidate_condensed.append(candidates_all[x+1])
+
+# Find the unique votes to each candidate
+    for i in range (len(candidate_condensed)):
+        votes_counter.append(candidates_all.count(candidate_condensed[i]))
+        voting_percent.append(round(((votes_counter[i] / total_votes) * 100), 3))
+       
+    most_votes = max(votes_counter)
+    winner = candidate_condensed[votes_counter.index(most_votes)]
+
+    print(f"Election Results")
+    print(f"------------------------------")
+    print(f"Total Votes: {total_votes}")
+    print(f"------------------------------")
+    print(f"{candidate_condensed[0]}: {voting_percent[0]} ({votes_counter[0]})")
+    print(f"{candidate_condensed[1]}: {voting_percent[1]} ({votes_counter[1]})")
+    print(f"{candidate_condensed[2]}: {voting_percent[2]} ({votes_counter[2]})")
+    print(f"------------------------------")
+    print(f"Winner: {winner}")
+    print(f"------------------------------")
+
+with open('analysis.txt', 'w' ) as datafile:
+    datafile.write(f"Election Results \n")
+    datafile.write(f"------------------------------ \n")
+    datafile.write(f"Total Votes: {total_votes} \n")
+    datafile.write(f"------------------------------ \n")
+    datafile.write(f"{candidate_condensed[0]}: {voting_percent[0]} ({votes_counter[0]}) \n")
+    datafile.write(f"{candidate_condensed[1]}: {voting_percent[1]} ({votes_counter[1]}) \n")
+    datafile.write(f"{candidate_condensed[2]}: {voting_percent[2]} ({votes_counter[2]}) \n")
+    datafile.write(f"------------------------------ \n")
+    datafile.write(f"Winner: {winner} \n")
+    datafile.write(f"------------------------------ \n")
+
+
+
+
