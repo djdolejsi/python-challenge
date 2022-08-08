7358f730f0e657181cebb8be7d6041ef1f393f07
Dillon Dolejsi <dillon41393@sbcglobal.net>
2022-08-08 09:47:19 -0500

    changes made on 8/8

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,8 +1,11 @@
+# Import dependencies
 import os
 import csv
 
+# Create the path for the CSV file
 csvpath = os.path.join('Resources/election_data.csv')
 
+# Create the list to store your data
 voting_id = []
 county = []
 candidates_all = []
@@ -10,33 +13,37 @@ candidate_condensed = []
 votes_counter = []
 voting_percent = []
 
-
-
+# Open your CSV file using the path you created
 with open(csvpath, encoding="utf-8") as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
 
+# identify the header in the data
     header = next(csvreader)
-# Adding all of the data in the voting_is, county and candidates_all lists
+
+# Looking through the data and adding all of the data in the voting_id, county and candidates_all lists
     for row in csvreader:
         voting_id.append(row[0])
         county.append(row[1])
         candidates_all.append(row[2])
 
+# Calculating the total votes
     total_votes = len(voting_id)
 
-# Removed duplicates from the candidiate all list to get a list of all the unique candidates   
+# Removed duplicates from the candidiate all list to get a list of all the unique candidates and putting that in the candidate_condensed list   
     for x in range(total_votes-1):
         if candidates_all[x+1] != candidates_all[x] and candidates_all[x+1] not in candidate_condensed:
             candidate_condensed.append(candidates_all[x+1])
 
-# Find the unique votes to each candidate
+# Find the unique votes to each candidate and voting percentage and adding them to the appropriate lists
     for i in range (len(candidate_condensed)):
         votes_counter.append(candidates_all.count(candidate_condensed[i]))
         voting_percent.append(round(((votes_counter[i] / total_votes) * 100), 3))
-       
+
+# Calculating who had the most votes in the election   
     most_votes = max(votes_counter)
     winner = candidate_condensed[votes_counter.index(most_votes)]
 
+# Print the results of the election
     print(f"Election Results")
     print(f"------------------------------")
     print(f"Total Votes: {total_votes}")
@@ -48,6 +55,7 @@ with open(csvpath, encoding="utf-8") as csvfile:
     print(f"Winner: {winner}")
     print(f"------------------------------")
 
+# # Create a txt file with my results and export it
 with open('analysis.txt', 'w' ) as datafile:
     datafile.write(f"Election Results \n")
     datafile.write(f"------------------------------ \n")
